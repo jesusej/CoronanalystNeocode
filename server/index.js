935cd429cf8479f5b201ac8b4b304a47bc75ea9b
jesusej <jesus-e98@hotmail.com>
2021-04-27 16:31:41 -0700

    :hammer: Adding hashing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const mysql = require("mysql");
 const cors = require ("cors");
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -62,14 +65,23 @@ app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query(
-        "INSERT INTO cuenta (Usuario, Contraseña, idTipo_De_Cuenta) VALUES (?, ?, 1)",
-        [username, password],
-        (err, result) => {
+    
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+
+        if(err) {
             console.log(err);
-            console.log(result);
         }
-    );
+        console.log(hash);
+
+        db.query(
+            "INSERT INTO cuenta (Usuario, Contraseña, idTipo_De_Cuenta) VALUES (?, ?, 1)",
+            [username, hash],
+            (err, result) => {
+                console.log(err);
+                console.log(result);
+            }
+        );
+    })
 });
 
 // Método GET de login que manda los datos de un usuario registrado si es que existe y si su sesión sigue activa
@@ -87,8 +99,8 @@ app.post('/login', (req, res) => {
     const password = req.body.password;
 
     db.query(
-        "SELECT * FROM cuenta WHERE Usuario = ? AND Contraseña = ?;",
-        [username, password],
+        "SELECT * FROM cuenta WHERE Usuario = ?;",
+        username,
         (err, result) => {
             if (err) {
                 res.send({err:err})
@@ -97,11 +109,18 @@ app.post('/login', (req, res) => {
             //console.log(result);
             // Envía los resultados como cookie
             if (result.length > 0) {
-                req.session.user = result;
-                console.log(req.session.user);
-                res.send(result);
+                bcrypt.compare(password, result[0].Contraseña, (error, response) => {
+                    console.log(response);
+                    if (response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
+                        res.send(result);
+                    } else {
+                        res.send({message: "No existe la combinación de correo y contraseña"});
+                    }
+                })
             } else {
-                res.send({message: "No existe la combinación de correo y contraseña"});
+                res.send({message: "El correo no existe"});
             }
         }
     );
